519a0e8029342a6ba416bce8b2a91bbe7d6e8870
Alex de Paula <alex.paula@viabilizei.com.br>
2020-04-17 00:05:52 -0300

    first commit

---
+++ HtmlToImage.API/Controllers/HtmlToImageController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PuppeteerSharp;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlToImage.API.Controllers
+{
+    [Route("api/HtmlToImage")]
+    [ApiController]
+    public class HtmlToImageController : ControllerBase
+    {
+        private readonly ILogger<HtmlToImageController> _logger;
+
+        public HtmlToImageController(ILogger<HtmlToImageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(PostHtmlToImageDTO dto)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (dto is null)
+            {
+                throw new ArgumentNullException(nameof(dto));
+            }
+
+#if true == DEBUG
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+#endif
+
+            ViewPortOptions defaultViewport = null;
+            if (dto.Width.HasValue && dto.Height.HasValue)
+                defaultViewport = new ViewPortOptions
+                {
+                    Width = dto.Width.Value,
+                    Height = dto.Height.Value
+                };
+
+            using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true,
+                Args = new[] { "--no-sandbox" },
+                DefaultViewport = defaultViewport
+            }))
+            using (var page = await browser.NewPageAsync())
+            {
+                string text = null;
+
+                if (!string.IsNullOrWhiteSpace(dto.HtmlBase64))
+                {
+                    var buff = Convert.FromBase64String(dto.HtmlBase64);
+                    text = UTF8Encoding.UTF8.GetString(buff);
+                }
+                else if (!string.IsNullOrWhiteSpace(dto.Html))
+                {
+                    text = dto.Html;
+                }
+
+                await page.SetContentAsync(text);
+                var image = await page.ScreenshotDataAsync();
+
+                return new FileContentResult(image, "image/png");
+            }
+        }
+    }
+}
---
+++ HtmlToImage.API/PostHtmlToImageDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HtmlToImage.API
+{
+    public class PostHtmlToImageDTO
+    {
+        public string HtmlBase64 { get; set; }
+        public string Html { get; set; }
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+    }
+}
